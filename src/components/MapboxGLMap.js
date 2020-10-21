90c3a0c6342f3e571b23354e464dda536263c0fb
Ane Sofie Smith Kristiansen <aneso@localhost.localdomain>
2020-10-21 17:55:28 +0200

    changed zoom and coordinates

--- src/components/MapboxGLMap.js
+++ src/components/MapboxGLMap.js
@@ -18,8 +18,8 @@ const MapboxGLMap = () => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-        center: [10.408773,63.422091],
-        zoom: 10
+        center: [10.7971737,59.7824265],
+        zoom: 15
       });
 
       map.on("load", () => {
